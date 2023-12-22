5e1369a39ea6154d991bc91bead27b712d7d022a
EidLars <eidenlars@gmail.com>
2023-12-22 02:59:30 +0100

    A2 aber kann den Controller nicht aufrufen

---
+++ app/Controllers/firstController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class firstController extends BaseController
+{
+    public function viewGruppennummer(): string
+    {
+        return var_dump(12);
+    }
+}
