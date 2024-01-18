d8a1669b117bb22e5a09ae09e4511d3597170e35
EidLars <eidenlars@gmail.com>
2024-01-18 12:29:31 +0100

    Clone von Chris V3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'TaskController::index');
+$routes->get('(:any)/viewGruppennummer', 'Start::viewGruppennummer');
+$routes->get('/Startseite', 'TaskController::index');
+$routes->get('/Spalten', 'TaskController::Spalten');
+$routes->get('/SpalteErstellen', 'TaskController::SpalteErstellen');
+$routes->get('/taskErstellen', 'TaskController::TaskErstellen');
+$routes->post('/createTask', 'TaskController::CreateTask');
+$routes->get('/taskBearbeiten', 'TaskController::TaskBearbeiten');
+$routes->get('/test', 'TaskController::test');
